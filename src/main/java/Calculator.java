5cc01f3a9786ea80959eca03d6e838f66bc41b6e
Erlantz Cabrejas <lantz1991@hotmail.com>
2024-01-22 14:42:31 +0000

    Added optionalDelimiter input check Removed empty strings when converting into Int

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,14 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Calculator {
-    static Integer add(String number){
-        if(number == null || number.isEmpty()){
+
+    public static Integer add(String input){
+        String optionalDelimiter = "";
+        Matcher matcher;
+
+        if(input == null || input.isEmpty()){
             return 0;
         }
 
-        String[] values = number.split("(,|\\n)+");
+        if(input.startsWith("//")){
+            matcher = checkIfDifferentDelimiterExists(input);
+            optionalDelimiter = matcher.group(1);
+            input = matcher.group(2);
+        }
 
+        String[] values = input.split("(,|\\n|"+optionalDelimiter+")+");
         int[] intValues = convertIntoInt(values);
+
+        if(intValues.length == 0){
+            return 0;
+        }
 
         if(intValues.length > 1){
             return IntStream.of(intValues).sum();
@@ -21,9 +38,25 @@ public class Calculator {
         int[] integerValues = new int[values.length];
 
         for(int i = 0; i < values.length; i++){
-            integerValues[i] = Integer.parseInt(values[i]);
+            if(!values[i].isEmpty()){
+                integerValues[i] = Integer.parseInt(values[i]);
+            }
         }
 
         return integerValues;
     }
+
+    public static Matcher checkIfDifferentDelimiterExists(String input){
+        String patternString = "/{2}(.)\\n(.*)";
+
+        Pattern pattern = Pattern.compile(patternString);
+
+        Matcher matcher = pattern.matcher(input);
+        boolean matches = matcher.matches();
+        String delimiter = matcher.group(1);
+        String rest = matcher.group(2);
+
+        return matcher;
+    }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -25,11 +25,19 @@ public class CalculatorTest {
 
     @Test
     public void testCommaAndNewLineDelimiterInput() {
-        //Assert.assertTrue(Calculator.add( ",1") == 1);
+        Assert.assertTrue(Calculator.add( ",1") == 1);
         Assert.assertTrue(Calculator.add( "1,") == 1);
         Assert.assertTrue(Calculator.add( "1,,,,,,7,,,,8") == 16);
         Assert.assertTrue(Calculator.add( "1\n2,3") == 6);
         Assert.assertTrue(Calculator.add("4\n9") == 13);
         Assert.assertTrue(Calculator.add( "1,\n8") == 9);
+    }
+
+    @Test
+    public void testNewOptionalDelimiterInput(){
+        Assert.assertTrue(Calculator.add("//;\n1;2") == 3);
+        Assert.assertTrue(Calculator.add("//%\n7%8%9%10") == 34);
+        Assert.assertTrue(Calculator.add("//1\n71819110") == 24);
+        Assert.assertTrue(Calculator.add("//1\n1111111") == 0);
     }
 }
