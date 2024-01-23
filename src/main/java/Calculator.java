f5db44bda0cbccda77963075f0b755bb4d9e7ab1
Erlantz Cabrejas <lantz1991@hotmail.com>
2024-01-23 16:55:34 +0000

    Fixed regex in split to include the optional delimiter and also keep the original split.
    Negative numbers trigger an exception and they are being displayed.
    
    Values higher than 1000 are excluded from the operation

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Calculator {
 
-    public static Integer add(String input){
+    public static Integer add(String input) throws Exception {
         String optionalDelimiter = "";
         Matcher matcher;
+        String[] values;
 
         if(input == null || input.isEmpty()){
             return 0;
@@ -18,9 +18,11 @@ public class Calculator {
             matcher = checkIfDifferentDelimiterExists(input);
             optionalDelimiter = matcher.group(1);
             input = matcher.group(2);
+            values = input.split("(,|\\n|"+optionalDelimiter+")+");
+        }else{
+            values = input.split("(,|\\n)+");
         }
 
-        String[] values = input.split("(,|\\n|"+optionalDelimiter+")+");
         int[] intValues = convertIntoInt(values);
 
         if(intValues.length == 0){
@@ -34,17 +36,29 @@ public class Calculator {
         return intValues[0];
     }
 
-    public static int[] convertIntoInt(String[] values){
+    public static int[] convertIntoInt(String[] values) throws Exception {
         int[] integerValues = new int[values.length];
+        ArrayList<String> negativeValues = new ArrayList<String>();
 
         for(int i = 0; i < values.length; i++){
-            if(!values[i].isEmpty()){
+            if(!values[i].isEmpty() && Integer.parseInt(values[i]) < 1001){
+                if(Integer.parseInt(values[i]) < 0){
+                    negativeValues.add(values[i]);
+                    continue;
+                }
+
                 integerValues[i] = Integer.parseInt(values[i]);
             }
         }
 
+        if(!negativeValues.isEmpty()){
+            throw new Exception("Negatives not allowed: "+negativeValues.toString());
+        }
+
         return integerValues;
     }
+
+
 
     public static Matcher checkIfDifferentDelimiterExists(String input){
         String patternString = "/{2}(.)\\n(.*)";
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,30 +1,34 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertThrows;
+
 public class CalculatorTest {
     @Test
-    public void testBlankValue() {
+    public void testBlankValue() throws Exception {
         Assert.assertNotNull(Calculator.add(""));
         Assert.assertTrue(Calculator.add("") == 0);
         Assert.assertTrue(Calculator.add(null) == 0);
     }
 
     @Test
-    public void testSingleValue() {
+    public void testSingleValue() throws Exception {
         Assert.assertTrue(Calculator.add("3") == 3);
         Assert.assertTrue(Calculator.add("7") == 7);
         Assert.assertTrue(Calculator.add("9") == 9);
     }
 
     @Test
-    public void testTwoValues() {
+    public void testTwoValues() throws Exception {
         Assert.assertTrue(Calculator.add("4,9") == 13);
         Assert.assertTrue(Calculator.add("9,7") == 16);
         Assert.assertTrue(Calculator.add("15,15") == 30);
     }
 
     @Test
-    public void testCommaAndNewLineDelimiterInput() {
+    public void testCommaAndNewLineDelimiterInput() throws Exception {
         Assert.assertTrue(Calculator.add( ",1") == 1);
         Assert.assertTrue(Calculator.add( "1,") == 1);
         Assert.assertTrue(Calculator.add( "1,,,,,,7,,,,8") == 16);
@@ -34,10 +38,68 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testNewOptionalDelimiterInput(){
+    public void testNewOptionalDelimiterInput() throws Exception {
         Assert.assertTrue(Calculator.add("//;\n1;2") == 3);
         Assert.assertTrue(Calculator.add("//%\n7%8%9%10") == 34);
         Assert.assertTrue(Calculator.add("//1\n71819110") == 24);
         Assert.assertTrue(Calculator.add("//1\n1111111") == 0);
+    }
+
+    @Test
+    public void testNegativeValuesInput(){
+
+        //First Case
+        Exception exception = assertThrows(Exception.class, () -> {
+            Calculator.add("-1,-2,-3,-4,-5");
+        });
+
+        ArrayList<String> negativeValues = new ArrayList<String>();
+        negativeValues.add("-1");
+        negativeValues.add("-2");
+        negativeValues.add("-3");
+        negativeValues.add("-4");
+        negativeValues.add("-5");
+
+        String expectedMessage = "Negatives not allowed: "+negativeValues.toString();
+        String actualMessage = exception.getMessage();
+
+        Assert.assertTrue(actualMessage.equals(expectedMessage));
+
+        //Second Case
+        exception = assertThrows(Exception.class, () -> {
+            Calculator.add("1,-2,3,-4,5");
+        });
+
+        negativeValues = new ArrayList<String>();
+        negativeValues.add("-2");
+        negativeValues.add("-4");
+
+        expectedMessage = "Negatives not allowed: "+negativeValues.toString();
+        actualMessage = exception.getMessage();
+
+        Assert.assertTrue(actualMessage.equals(expectedMessage));
+
+
+        //Third Case
+        exception = assertThrows(Exception.class, () -> {
+            Calculator.add("-11,27,-34,4,-99");
+        });
+
+        negativeValues = new ArrayList<String>();
+        negativeValues.add("-11");
+        negativeValues.add("-34");
+        negativeValues.add("-99");
+
+        expectedMessage = "Negatives not allowed: "+negativeValues.toString();
+        actualMessage = exception.getMessage();
+
+        Assert.assertTrue(actualMessage.equals(expectedMessage));
+    }
+
+    @Test
+    public void testValuesHigherThan1000() throws Exception {
+        Assert.assertTrue(Calculator.add("1001,2") == 2);
+        Assert.assertTrue(Calculator.add("//;\n1;10000") == 1);
+        Assert.assertTrue(Calculator.add("//%\n7%8000%9000%10") == 17);
     }
 }
